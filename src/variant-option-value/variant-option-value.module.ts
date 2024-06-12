2c49eb5fd583d6a289df1846897a12863cbf5900
phuochungus <21520252@gm.uit.edu.vn>
2024-06-12 21:18:41 +0700

    chore: Add Dockerfile and .dockerignore for containerization

--- src/variant-option-value/variant-option-value.module.ts
+++ src/variant-option-value/variant-option-value.module.ts
@@ -1,9 +1,5 @@
 import { Module } from '@nestjs/common';
-import { VariantOptionValueService } from './variant-option-value.service';
-import { VariantOptionValueController } from './variant-option-value.controller';
 
-@Module({
-  controllers: [VariantOptionValueController],
-  providers: [VariantOptionValueService],
-})
-export class VariantOptionValueModule {}
+
+@Module({})
+export class VariantOptionValueModule { }
